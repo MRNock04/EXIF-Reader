8465a7fd63a69203e037a311be940389a60e02ee
Mika <uttzi@student.kit.edu>
2024-02-08 14:54:44 +0100

    fghjuio

--- reader.py
+++ reader.py
@@ -1,2 +1,15 @@
 import numpy as np
 import os
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+image = Image.open('MUM_5432-Edit.jpg')
+exif = image._getexif()
+
+data = {}
+
+for key, val in exif.items():
+    if key in TAGS:
+        data[TAGS[key]] = val
+
+print(data['Software'])
